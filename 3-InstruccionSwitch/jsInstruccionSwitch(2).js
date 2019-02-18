fc1af0663cf202bc5324480a1cba9e46e40d3d90
nimref149 <47321357+nimref149@users.noreply.github.com>
2019-02-18 12:34:06 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,29 @@
-function Mostrar()
+function mostrar()
 {
-//tomo el mes 
+//tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAño)  {
+    case "Julio":
+    case "Agosto":
+    alert("Abrigate que hace frio.");
+break;
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+    alert("Falta para el invierno.");
+break;
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+     alert("Felices fiesta!!!");
+break;
+
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,11 +1,30 @@
-function Mostrar()
+function mostrar()
 {
-//tomo el mes 
+//tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+switch(mesDelAño) {
+case "Febrero":
+alert("tiene 28 dias");
+break;
+case "Abril":
+case "Junio":
+case "Septiembre":
+case "Noviembre":
+alert("Tiene 30 dias");
+break;
+case "Enero":
+case "Marzo":
+case "Mayo":
+case "Julio":
+case "Agosto":
+case "Octubre":
+case "Diciembre":
+alert("Tiene 31 dias");
+break;
+
+}
 
 
 
